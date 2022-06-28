2acca7d5c58a4d87949904f041d96c7066da9fa0
gamg <adolfz10@gmail.com>
2022-06-28 12:26:48 -0400

    password was changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\SocialLink;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
          \App\Models\User::factory()->create([
              'name' => 'Gustavo',
              'email' => 'adolfz10@gmail.com',
+             'password' => Hash::make('*77.Carlotaz.77*'),
          ]);
 
          \App\Models\Navitem::factory()->create([
