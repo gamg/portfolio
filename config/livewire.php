4e83ce93a9f3a8c5523982106e5af95b0c5872ce
gamg <adolfz10@gmail.com>
2022-07-01 20:18:24 -0400

    test

--- config/livewire.php
+++ config/livewire.php
@@ -97,7 +97,7 @@ return [
 
     'temporary_file_upload' => [
         'disk' => 'google',        // Example: 'local', 's3'              Default: 'default'
-        'rules' => 'image|max:1024',       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
+        'rules' => ['image','max:1024'],       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => 'tmp',   // Example: 'tmp'                      Default  'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
